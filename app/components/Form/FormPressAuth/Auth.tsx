72b84f140aab6c0d19420003af012410a1f0db72
Vadim Matveev <matveev.vadim27@gmail.com>
2025-10-19 19:12:22 +0300

    feat: add block user window

--- app/components/Form/FormPressAuth/Auth.tsx
+++ app/components/Form/FormPressAuth/Auth.tsx
@@ -1,9 +1,11 @@
 import { useLoginMutation } from '~/api/controllers/auth';
 import type { ILoginArgs } from '~/api/controllers/auth/types';
+import ModalBlockedJournalist from '~/components/Modal/ModalBlockedJournalist';
 import Button from '~/components/ui/Button';
 import Input from '~/components/ui/Input';
 import { RouterPaths } from '~/routes';
 import { setProfileData, setUserData, setUserToken } from '~/store/authSlice';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
@@ -12,12 +14,14 @@ export default function Auth({ setIsRestore }: { setIsRestore: () => void }) {
   const dispatch = useDispatch();
   const [login, { isLoading }] = useLoginMutation();
   const navigate = useNavigate();
+  const [isBlocked, setIsBlocker] = useState<boolean>(false);
 
   const {
     register,
     handleSubmit,
     setError,
     clearErrors,
+    reset,
     formState: { errors, isValid },
   } = useForm<ILoginArgs>();
 
@@ -39,57 +43,68 @@ export default function Auth({ setIsRestore }: { setIsRestore: () => void }) {
         }
       })
       .catch(error => {
-        console.error('Ошибка:', error);
-        setError('root', {
-          type: 'manual',
-          message: error?.data?.message || 'Неверный логин или пароль',
-        });
+        if (error?.status === 403) {
+          setIsBlocker(true);
+          reset();
+        } else {
+          setError('root', {
+            type: 'manual',
+            message: error?.data?.message || 'Неверный логин или пароль',
+          });
+        }
       });
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      noValidate
-      className="flex flex-col md:w-md"
-    >
-      <h2 className="font-(--font-halvar) text-4xl mb-8">Вход</h2>
+    <>
+      {!isBlocked && (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          className="flex w-full flex-col "
+        >
+          <h2 className="font-(--font-halvar) text-4xl mb-8">Вход</h2>
+          <Input
+            type="text"
+            label="Email или номер телефона"
+            {...register('email', { required: 'Это поле обязательно' })}
+            error={!!errors.email?.message}
+            autoComplete="username"
+            classNames="mb-4"
+          />
 
-      <Input
-        type="text"
-        label="Email или номер телефона"
-        {...register('email', { required: 'Это поле обязательно' })}
-        error={!!errors.email?.message}
-        autoComplete="username"
-        classNames="mb-4"
-      />
+          <Input
+            type="password"
+            label="Пароль"
+            {...register('password', { required: 'Это поле обязательно' })}
+            error={!!errors.password?.message}
+            autoComplete="current-password"
+            classNames="mb-6"
+          />
 
-      <Input
-        type="password"
-        label="Пароль"
-        {...register('password', { required: 'Это поле обязательно' })}
-        error={!!errors.password?.message}
-        autoComplete="current-password"
-        classNames="mb-6"
-      />
+          <button
+            type="button"
+            className="mb-8 cursor-pointer"
+            onClick={setIsRestore}
+          >
+            Забыли пароль?
+          </button>
 
-      <button
-        type="button"
-        className="mb-8 cursor-pointer"
-        onClick={setIsRestore}
-      >
-        Забыли пароль?
-      </button>
+          {errors.root && (
+            <div className="text-red-500 text-sm mb-4 md:mb-8">
+              {errors.root.message}
+            </div>
+          )}
 
-      {errors.root && (
-        <div className="text-red-500 text-sm mb-4 md:mb-8">
-          {errors.root.message}
-        </div>
+          <Button type="submit" disabled={isLoading || !isValid}>
+            {isLoading ? 'Входим...' : 'Продолжить'}
+          </Button>
+        </form>
       )}
 
-      <Button type="submit" disabled={isLoading || !isValid}>
-        {isLoading ? 'Входим...' : 'Продолжить'}
-      </Button>
-    </form>
+      {isBlocked && (
+        <ModalBlockedJournalist onChange={() => setIsBlocker(false)} />
+      )}
+    </>
   );
 }
